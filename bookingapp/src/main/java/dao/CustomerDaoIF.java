23d2b9294afbd7e551e1736deb7716697c47e524
Saurabh-0102 <91018211+Saurabh-0102@users.noreply.github.com>
2022-01-25 00:24:30 +0530

    Delete bookingapp directory

--- bookingapp/src/main/java/dao/CustomerDaoIF.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public interface CustomerDaoIF {
-
-	public Object addCustomer(Object obj);
-	public Object getCustomer(String email,String password);
-	public Object getAllCustomer();
-}
